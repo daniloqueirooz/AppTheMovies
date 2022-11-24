ea5b4b4bfaac8316090271ba00124cea63ae3884
daniloqueirooz <danilodequeirozq@gmail.com>
2022-11-23 22:13:41 -0300

    resolving fragment conflicts

--- app/src/main/java/com/example/appthemovies/adapter/TheMovieAdapter.kt
+++ app/src/main/java/com/example/appthemovies/adapter/TheMovieAdapter.kt
@@ -14,7 +14,7 @@ import com.example.appthemovies.data.DataSource
 
 class TheMovieAdapter(
 
-    private val layout: Int
+    private var layout: Int
 
 ) : RecyclerView.Adapter<TheMovieAdapter.TheMovieHolder>() {
 
