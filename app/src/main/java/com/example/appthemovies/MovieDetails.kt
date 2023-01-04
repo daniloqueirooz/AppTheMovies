a0b023607115b46cada5b1e54877fa418ffb40b4
daniloqueirooz <danilodequeirozq@gmail.com>
2023-01-04 10:44:36 -0300

    updating bug fixes toast

--- app/src/main/java/com/example/appthemovies/MovieDetails.kt
+++ app/src/main/java/com/example/appthemovies/MovieDetails.kt
@@ -6,11 +6,16 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.appthemovies.databinding.FragmentMovieDetailsBinding
 
-class MovieDetails : Fragment() {
+class MovieDetails : Fragment(R.layout.fragment_movie_details) {
+
 
     private var _binding: FragmentMovieDetailsBinding? = null
 
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
